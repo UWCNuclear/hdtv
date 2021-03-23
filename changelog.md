c0d81d72d4a0bf89619581004bbe58f8644c7684
nikitabern <7242807+nikitabern@users.noreply.github.com>
2021-03-23 15:20:55 +0200

    Add files via upload

---
+++ changelog.md
@@ -0,0 +1,52 @@
+# HDTV Change Log
+
+## 18.01
+- Changed building process in setup.py to allow for package building
+
+## 17.12
++ (Some) Support for Integrals: If one wants to work exclusively with integrals, it is possible to work with peak less fits. See e.g. `fit integral list`. (@opapst)
++ Support for Python3 (@warr, @opapst) (Closes #12)
++ Automatic recompilation on ROOT version changes (@jmayer)
++ Easier installation, e.g. via pip (@jmayer)
++ Switch from optparse to argparse (@opapst) (Closes #15)
++ Switch from own implementation of error values to uncertainties libary (@opapst) (Closes #14)
++ pytest based testing (@opapst)
+- Changes to diplay of tables and uncertainties, see `config set table help`, and `config set uncertainties help` (@opapst)
+- Improvements to calibration fitting procedure (@opapst)
+- Improvements to tab completion and command line interface (@opapst)
+- A bit of documentation (#6)
+- Replace deprecated auto_ptr (@warr)
+- Allow exiting the program from batch files and other operations (@opapst)
+- Missing background markers: Unexpected behavior fixed (@opapst)
+- Diverse other Fixes (@opapst, @warr, @jmayer)
+- Change location for user settings/history (@opapst)
+
+## 17.01
++ Relative efficiencies limited to energy range of absolute efficiency (@ehoemann)
++ Fix Printing (@warr)
+
+## 16.12
++ Fixed cutting calibrated mfile matrices
++ Fixed wide tables errvalue
+
+## 16.11
+- Fixed spectrum subtract crashing
++ Added support to add/subtract calibrated spectra
++ Added command `spectrum calbin` to rebin spectra to calibration unit.
+- Fixed rebinning sometimes not updating calibration
+
+## 16.10
++ wide tables: `config set table wide` for easier Copy & Pasting of tabular data. Use `config set table classic` to change back
++ Nuclide Decay Databases: e.g., `nuclide Ra-226` (@ehoemann)
++ Efficiency Fitting `calibration efficiency fit` (@ehoemann)
++ Some Readme
+- Fixed (TM) installation process
+- Removed a lot of dead code
+- Cleaner startup output
+
+## 16.09
+- Fixed Peakfinder not working sometimes
+- Support negative calibrations
+- Fixed spectrum integration not working if higher region markers was set first
+- Fixed rebinning spectra causing integration to sometime return the wrong volume
+- Changes to installation and loading path
---
+++ setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import hdtv.version
+from setuptools import setup
+from distutils.command.build import build
+import glob
+import subprocess
+import os
+
+class CustomBuild(build):
+    def run(self):
+        # run original install code
+        build.run(self)
+
+        # Build libraries for this system
+        # run hdtv --rebuild-sys after installation or any root version change
+        # or reinstall from scratch
+        for module in ['mfile-root', 'fit', 'display']:
+            dir = os.path.join(self.build_lib, 'hdtv/rootext/', module)
+            print("Building library in %s" % dir)
+            subprocess.check_call(['make', '-j', '--silent'], cwd=dir)
+
+manpages = glob.glob('doc/guide/*.1')
+
+setup(
+    name='hdtv',
+    version=hdtv.version.VERSION,
+    description='HDTV - Nuclear Spectrum Analysis Tool',
+    url='https://gitlab.ikp.uni-koeln.de/staging/hdtv',
+    maintainer='Jan Mayer',
+    maintainer_email='jan.mayer@ikp.uni-koeln.de',
+    license='GPL',
+    install_requires=['scipy', 'matplotlib'],
+    extras_require={
+        'dev': ['docutils'],
+        'test': ['pytest'],
+    },
+    scripts=['bin/hdtv'],
+    packages=[
+        'hdtv',
+        'hdtv.plugins',
+        'hdtv.peakmodels',
+        'hdtv.efficiency',
+        'hdtv.database',
+        'hdtv.rootext',
+    ],
+    package_data={
+        'hdtv': ['share/*'],
+        'hdtv.rootext': [
+            'mfile-root/*.h', 'mfile-root/*.cxx', 'mfile-root/Makefile',
+            'mfile-root/libmfile-root.so', 'mfile-root/libmfile-root_rdict.pcm', 'mfile-root/libmfile-root.rootmap',
+            'mfile-root/matop/*.h', 'mfile-root/matop/*.c',
+            'fit/*.h', 'fit/*.cxx', 'fit/Makefile',
+            'fit/libfit.so', 'fit/libfit_rdict.pcm', 'fit/libfit.rootmap',
+            'display/*.h', 'display/*.cxx', 'display/Makefile',
+            'display/libdisplay.so', 'display/libdisplay_rdict.pcm', 'display/libdisplay.rootmap',
+        ],
+    },
+    data_files=[
+        ('share/man/man1', manpages),
+        ('share/zsh/site-functions', ['data/completions/_hdtv']),
+        ('share/bash-completion/completions', ['data/completions/hdtv']),
+        ('share/applications', ['data/hdtv.desktop']),
+    ],
+    cmdclass={
+        'build': CustomBuild,
+    }
+)
